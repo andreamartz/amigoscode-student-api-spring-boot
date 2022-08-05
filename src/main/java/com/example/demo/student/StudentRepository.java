e958417dc34dca43aea999847f1b417a9fe77862
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-08-04 20:42:57 -0500

    Finish POST, add DELETE and PUT mappings

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -3,8 +3,12 @@ package com.example.demo.student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 // This is part of the Data Access Layer
 @Repository  // tells Spring that this interface is responsible for data access
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
+    // the following is equivalent to the following JPQL code:
+        // @Query("SELECT s FROM Student s WHERE s.email = ?1");
+    Optional<Student> findStudentByEmail(String email);
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service  // exactly like @Component, but more semantic
     // @Service tells Spring that studentService is a class that has to be instantiated, i.e. it has to be a Spring bean
@@ -24,6 +26,46 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException(
+                "student with id " + studentId + " does not exist"
+            );
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional  // means that you don't have to implement any JPQL query
+    public void updateStudent(Long studentId, String name, String email) {
+        Optional<Student> studentOptional = studentRepository.findById(studentId);
+        if (studentOptional.isEmpty()) {
+            throw new IllegalStateException(
+                "student with id " + studentId + " does not exist"
+            );
+        }
+
+        Student student = studentOptional.get();
+
+        if (name != null && name.length() > 0 && student.getName() != name) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && student.getEmail() != email) {
+            Optional<Student> studentOptional1 = studentRepository.findStudentByEmail(email);
+            if (studentOptional1.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
+
+        studentRepository.save(student);
     }
 }
